ce1555633a1709cbe0a20681595dd0fd978f4652
farrell@gonicus.de <farrell@gonicus.de>
2024-02-02 11:08:31 +0100

    Removed all but the zuul role

--- roles/nexus/files/setup_realms.groovy
+++
@@ -1,9 +0,0 @@
-import groovy.json.JsonSlurper
-import org.sonatype.nexus.security.realm.RealmManager
-
-parsed_args = new JsonSlurper().parseText(args)
-
-realmManager = container.lookup(RealmManager.class.getName())
-
-// enable/disable the Docker Bearer Token Realm
-realmManager.enableRealm("DockerToken", parsed_args.docker_bearer_token_realm)
